ebcd56cb7b0ebb4f65e723f142d4089022ef9c28
virtualaura <20338026+virtualaura@users.noreply.github.com>
2025-03-25 09:52:32 +0100

    Updates

---
+++ precache-manifest.b3d1c402636680b91755c4362dc59199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "033b530b70eac05ee272c5bf0f0a83ae",
+    "url": "/specialweek-2025/index.html"
+  },
+  {
+    "revision": "461b0a8b91e569e4225c",
+    "url": "/specialweek-2025/static/css/main.c6001462.chunk.css"
+  },
+  {
+    "revision": "0b3469c690fd4a390570",
+    "url": "/specialweek-2025/static/js/2.6d9af362.chunk.js"
+  },
+  {
+    "revision": "461b0a8b91e569e4225c",
+    "url": "/specialweek-2025/static/js/main.5caf1d18.chunk.js"
+  },
+  {
+    "revision": "0aafafe3212c6a4ae7dd",
+    "url": "/specialweek-2025/static/js/runtime~main.328df5c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/specialweek-2025/precache-manifest.7990aa1e7e4a9ec6cd1076b0b5909503.js"
+  "/specialweek-2025/precache-manifest.b3d1c402636680b91755c4362dc59199.js"
 );
 
 self.addEventListener('message', (event) => {
