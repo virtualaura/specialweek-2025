5dd41c06b43f3fa79602283c732b589f96e61811
virtualaura <20338026+virtualaura@users.noreply.github.com>
2025-03-25 07:54:52 +0100

    Updates

---
+++ precache-manifest.3f6472af9fb297eeeaec4d010afc2061.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5585a2ae4a04c49a56b7f11ebcd1e79",
+    "url": "/virtualaura/specialweek-2025/index.html"
+  },
+  {
+    "revision": "13ae667149d3b39407fc",
+    "url": "/virtualaura/specialweek-2025/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/virtualaura/specialweek-2025/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/virtualaura/specialweek-2025/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "13ae667149d3b39407fc",
+    "url": "/virtualaura/specialweek-2025/static/js/main.554c90a6.chunk.js"
+  },
+  {
+    "revision": "7b89fd5f5e461e1a6ab5",
+    "url": "/virtualaura/specialweek-2025/static/js/runtime~main.4e72accb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/virtualaura/specialweek-2025/static/media/logo.06e73328.svg"
+  }
+]);
