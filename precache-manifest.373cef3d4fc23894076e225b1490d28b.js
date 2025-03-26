5cb9c4cf8c94d4c7cb6acaa70d7b1647ea4c5bd9
virtualaura <20338026+virtualaura@users.noreply.github.com>
2025-03-26 09:08:20 +0100

    Updates

--- precache-manifest.373cef3d4fc23894076e225b1490d28b.js
+++ precache-manifest.373cef3d4fc23894076e225b1490d28b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "959d460b794522a27948db0e18498713",
+    "revision": "e8d42e891f9f04f4ff79722a77b5a6c1",
     "url": "/specialweek-2025/index.html"
   },
   {
-    "revision": "d033742142ed172fcfea",
-    "url": "/specialweek-2025/static/css/main.730d4eaa.chunk.css"
+    "revision": "df4c35ae98b83be401ca",
+    "url": "/specialweek-2025/static/css/main.19efd81e.chunk.css"
   },
   {
     "revision": "7490e3e5de734d226450",
     "url": "/specialweek-2025/static/js/2.2f398534.chunk.js"
   },
   {
-    "revision": "d033742142ed172fcfea",
-    "url": "/specialweek-2025/static/js/main.8ca9ab88.chunk.js"
+    "revision": "df4c35ae98b83be401ca",
+    "url": "/specialweek-2025/static/js/main.f6227a1e.chunk.js"
   },
   {
     "revision": "0aafafe3212c6a4ae7dd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/specialweek-2025/precache-manifest.edb3be022ab354a0c1f9ef69aa8fe1f6.js"
+  "/specialweek-2025/precache-manifest.373cef3d4fc23894076e225b1490d28b.js"
 );
 
 self.addEventListener('message', (event) => {
